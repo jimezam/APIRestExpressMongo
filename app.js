13cb0aa97ba7e4379021e1650480ea4a67a679b8
Jorge I. Meza <jimezam@gmail.com>
2018-05-06 17:19:59 -0500

    Express API server base

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+/// Remover
+/// var indexRouter = require('./routes/index');
+/// var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+
+/// Remover
+/// // view engine setup
+/// app.set('views', path.join(__dirname, 'views'));
+/// app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +22,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/// Remover 
+/// app.use('/', indexRouter);
+/// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,9 +37,10 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  /// Remover
+  /// // render the error page
+  /// res.status(err.status || 500);
+  /// res.render('error');
 });
 
 module.exports = app;
