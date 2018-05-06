a09c145afeed3a408058337e6c44ea01d5c482cd
Jorge I. Meza <jimezam@gmail.com>
2018-05-06 17:38:57 -0500

    Demo (Hello World)  GET / service

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
+"use strict"        // add
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
+  res.status(200)   // add
+     .json({'message': 'Hello World'});
+
   /// Remover:
   /// res.render('index', { title: 'Express' });
 });
