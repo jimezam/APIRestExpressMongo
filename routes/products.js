cf6e6e2e8af9dbec14f607e40c64376bee804820
Jorge I. Meza <jimezam@gmail.com>
2018-05-06 22:42:15 -0500

    Modified PUT a Product to use MongoDB/Mongoose

--- routes/products.js
+++ routes/products.js
@@ -103,7 +103,9 @@ router.get('/:id', function(req, res, next) {
 
     // Mongoose
 
-    // db.products.find({ _id: ObjectId(5aefa0b961e1cc2194b5ced5) });
+    // Doc: https://docs.mongodb.com/manual/reference/method/db.collection.findOne/
+
+    // db.products.find({ _id: ObjectId("5aefa0b961e1cc2194b5ced5") });
 
     Product.findOne({ _id: req.params.id })
         .then(product => {
@@ -139,7 +141,7 @@ router.get('/:id', function(req, res, next) {
 /* PUT a particular product. */
 router.put('/:id', function(req, res, next) {
     console.log("Product.PUT: " + req.params.id);
-  
+
     if(!req.body)
     {
       res.status(403)
@@ -157,13 +159,37 @@ router.put('/:id', function(req, res, next) {
               "message": "There is no 'id' parameter!"
           });
     }
+    
+    // Mongoose
 
+    // Doc: https://docs.mongodb.com/manual/reference/method/db.collection.findOneAndUpdate/
+
+    // example?
+
+    Product.findOneAndUpdate({"_id": req.params.id}, {$set: req.body}, {new: true})
+        .then(product => {
+            res.status(200)
+               .json({"product": product});
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(404)
+               .json({
+                   "error": true,
+                   "message": err
+            });     
+    });
+
+    // /Mongoose    
+
+    /*    
     let product = req.body;
     product._id = parseInt(req.params.id, 10);
     products[product._id] = product;
 
     res.status(200)
        .json({"product": product});
+    */
 });
 
 /* DELETE a particular product. */
